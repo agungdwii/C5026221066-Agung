4a8926695e44ca9c7168b986aaa9e96281d919d2
Agung Dwi Oktavian <118071797+agungdwii@users.noreply.github.com>
2023-11-16 11:45:05 +0700

    modif footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -632,17 +632,7 @@
     <!-- Footer End -->
 
 
-    <!-- Copyright Start -->
-    <div class="container-fluid copyright py-4">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a class="border-bottom" href="#">Akang Desain</a>, All Right Reserved.
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Copyright End -->
+    
 
 
     <!-- Back to Top -->
