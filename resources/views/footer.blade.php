20e774b9a4a979a7c140ff3e8f8861a95fa13206
Agung Dwi Oktavian <118071797+agungdwii@users.noreply.github.com>
2023-11-30 15:05:30 +0700

    Pertemuan 30 Nop 2023

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<br/>
+    <br/>
+    <hr/>
+    <footer>
+        <p>&copy; Muhammad Rafli Krishnadanu.2023</p>
+    </footer>
--- routes/web.php
+++ routes/web.php
@@ -17,48 +17,61 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('halo', function () {
+    //Bisa diisi program apa saja
+    return "Halo Apa kabar" ;
+});
+
+Route::get('halo2', function () {
+    return "<h1>Halo Apa kabar</h1>" ;
+});
+
 Route::get('blog', function () {
-    return view('blog');
+	return view('blog');
 });
 
-Route::get('file1', function () {
-    return view('file1');
+Route::get('blog', function () {
+	return view('blog');
 });
 
-Route::get('file2', function () {
-    return view('file2');
+Route::get('hello', function () {
+	return view('hello');
 });
 
-Route::get('file3', function () {
-    return view('file3');
+Route::get('link', function () {
+	return view('link');
 });
 
-Route::get('file4', function () {
-    return view('file4');
+Route::get('style', function () {
+	return view('style');
 });
 
-Route::get('file5', function () {
-    return view('file5');
+Route::get('responsive', function () {
+	return view('responsive');
 });
 
-Route::get('file6', function () {
-    return view('file6');
+Route::get('responsive2', function () {
+	return view('responsive2');
 });
 
-Route::get('file7', function () {
-    return view('file7');
+Route::get('js1', function () {
+	return view('js1');
 });
 
-Route::get('tugas1', function () {
-    return view('tugas1');
+Route::get('js2', function () {
+	return view('js2');
 });
 
-Route::get('tugas2', function () {
-    return view('tugas2');
+Route::get('company', function () {
+	return view('company');
 });
 
-Route::get('tugas3', function () {
-    return view('tugas3');
+Route::get('linktree', function () {
+	return view('linktree');
+});
+
+Route::get('card', function () {
+	return view('card');
 });
 
 Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
@@ -72,15 +85,14 @@ Route::get('formulir', 'App\Http\Controllers\DosenController@formulir');
 Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
 
 Route::get('/blog', function () {
-    return view('home');
+	return view('home');
 });
 
 Route::get('/blog/tentang', function () {
-    return view('tentang');
+	return view('tentang');
 });
-
 Route::get('/blog/kontak', function () {
-    return view('kontak');
+	return view('kontak');
 });
 
 //route CRUD
@@ -90,3 +102,4 @@ Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
 Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
 Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
 Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
+Route::get('/pegawai/cari','App\Http\Controllers\PegawaiController@cari');
